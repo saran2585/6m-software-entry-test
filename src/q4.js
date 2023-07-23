dc768a84a0cb34ea74a62570d7f74418cf3db6b4
saran2585 <113222897+saran2585@users.noreply.github.com>
2023-07-24 00:47:42 +0800

    Update q4.js

--- src/q4.js
+++ src/q4.js
@@ -23,7 +23,19 @@ class Parent{
 const p = new Parent();
 p.showAbilities(); // Observe that this function prints "Parenting" and "Role modeling".
 
-// Task 1: Add code here
+// Task 1: Add code here - Add a constructor to the Child class can calls super().
+class Child extends Parent {
+    // Adding a constructor
+    constructor() {
+        super(); // to invoke parent's constructor. 
+    }
+
+    // Implement a new function addNewAbilities(newAbility) in the Child class where the new ability will be added to the Parent's #abilities array.     
+    addNewAbility(ability) {
+        this.abilities.push(ability);
+    }
+}
+
 
 const c = new Child();
 c.addNewAbility("Dancing");
